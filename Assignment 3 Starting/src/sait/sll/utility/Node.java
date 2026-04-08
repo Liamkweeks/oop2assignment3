2f579d0e14e3b040e8efb878959d7e37f6281ee8
Liam <liamkweeks@gmail.com>
2026-04-08 13:26:26 -0600

    Added missing classes in utility package, etc
    See above, also uncommented a line in the serialization test, added some of the node structure and the SLL structure, implemented serializable in user, SLL, and Node classes. added needed imports in said classes where necessary

---
+++ Assignment 3 Starting/src/sait/sll/utility/Node.java
@@ -0,0 +1,16 @@
+package sait.sll.utility;
+
+import java.io.Serializable;
+
+public class Node implements Serializable
+{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -8162557647406239527L;
+	private Object data;
+	private Node next;
+	
+
+
+}
